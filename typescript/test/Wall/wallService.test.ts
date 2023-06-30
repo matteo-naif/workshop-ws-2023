047654537cada4e52712e440e8108200eb4bf6b8
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2023-06-30 15:02:36 +0200

    First test

--- typescript/test/Wall/wallService.test.ts
+++ typescript/test/Wall/wallService.test.ts
@@ -1,5 +1,17 @@
+import { User } from "../../src/User/User";
+import { WallService } from "../../src/Wall/WallService";
+
 describe('Wall Service test', () => {
     it('Testing library works!', () => {
         expect(true).toEqual(true);
     });
+
+    describe('Call with empty User', () => {
+        it('Should throw an exception', () => {
+            const user = new User();
+            const wallService = new WallService();
+            expect(() => { wallService.anotherBrickInTheWall(user, 'message') }).toThrow()
+        })
+    });
 });
+
